735b92b7260683341ccd4fcb19b0c2a09da08989
Alexis Piéplu <alexis.pieplu@savoirfairelinux.com>
2016-07-14 16:42:27 -0400

    use new helper function queryAll

--- slick/slick.js
+++ slick/slick.js
@@ -340,7 +340,7 @@
             asNavFor = _.options.asNavFor;
 
         if ( asNavFor && asNavFor !== null ) {
-            asNavFor = [].slice.call(document.querySelectorAll(asNavFor)).filter(function(elem, index, array){
+            asNavFor = _.queryAll(asNavFor).filter(function(elem, index, array){
                 return elem !== _.$slider.get(0);
             });
             asNavFor = $(asNavFor);
