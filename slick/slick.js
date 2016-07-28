3272a69c5ad1a5882a7044c6b08c10b33f600426
Stefan Langenmaier <stefan.langenmaier@savoirfairelinux.com>
2016-07-28 12:51:26 -0400

    activateADA : removed Arrary.from method

--- slick/slick.js
+++ slick/slick.js
@@ -191,9 +191,9 @@ Issues: http://github.com/kenwheeler/slick/issues
 		// TODO remove this line once $slideTrack is no longer a jquery object
 		var _slideTrack = _.$slideTrack.get(0); // assuming there is only one track
 
-		Array.from(_slideTrack.querySelectorAll('.slick-active')).forEach(function(element, index, array){
+		_.queryAll('.slick-active', _slideTrack).forEach(function(element, index, array){
 			element.setAttribute('aria-hidden', 'false');
-			Array.from(element.querySelectorAll('a, input, button, select')).forEach(function(element2, index2, array2){
+			_.queryAll('a, input, button, select', element).forEach(function(element2, index2, array2){
 				element2.setAttribute('tabindex', '0');
 			});
 		});
