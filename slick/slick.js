49fd723c66292f0952072f0026763efbb512f8a4
Amélie Turgeon <amtur8@users.noreply.github.com>
2016-08-25 13:44:24 -0400

    Revert "Revert "method activateADA""

--- slick/slick.js
+++ slick/slick.js
@@ -188,10 +188,14 @@ Issues: http://github.com/kenwheeler/slick/issues
 	Slick.prototype.activateADA = function() {
 		var _ = this;
 
-		_.$slideTrack.find('.slick-active').attr({
-			'aria-hidden': 'false'
-		}).find('a, input, button, select').attr({
-			'tabindex': '0'
+		// TODO remove this line once $slideTrack is no longer a jquery object
+		var _slideTrack = _.$slideTrack.get(0); // assuming there is only one track
+
+		_.queryAll('.slick-active', _slideTrack).forEach(function(element, index, array){
+			element.setAttribute('aria-hidden', 'false');
+			_.queryAll('a, input, button, select', element).forEach(function(element2, index2, array2){
+				element2.setAttribute('tabindex', '0');
+			});
 		});
 
 	};
